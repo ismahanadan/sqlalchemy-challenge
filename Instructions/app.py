66dfb864e91be3db46f05e168013f659552fd853
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2021-01-27 15:40:19 -0500

    adding code to app.py

--- Instructions/app.py
+++ Instructions/app.py
@@ -24,7 +24,58 @@ Measurement = Base.classes.measurement
 
 app = Flask(__name__)
 
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+    )
+
+
 # FLask routes
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    #Convert the query results to a dictionary using date as the key and prcp as the value
+    all_data = []
+
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"]= date
+        prcp_dict["prcp"]= prcp
+        all_data.append(prcp_dict)
+
+    return jsonify(all_data)
+    
+        
 
 
 
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="127.0.0.1",port=8060)

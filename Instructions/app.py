e334174ea466fd68d383168214f10777df789c85
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2021-01-27 01:46:19 -0500

    adding code

---
+++ Instructions/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#set up database
+
+engine = create_engine("sqlite:///./Resources/hawaii.sqlite")
+
+#reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+
+#set up Flask
+
+app = Flask(__name__)
+
+# FLask routes
+
+
+
